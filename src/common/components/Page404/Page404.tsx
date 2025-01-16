163a9421c90682b5dd20e4db2726d06d92a89a63
renata-and <renata.androsova@gmail.com>
2025-01-16 19:53:32 +0700

    Added Home button to Page404

--- src/common/components/Page404/Page404.tsx
+++ src/common/components/Page404/Page404.tsx
@@ -1,10 +1,16 @@
 import s from "./Page404.module.css"
+import Button from "@mui/material/Button"
+import { Link } from "react-router"
+import { PATH } from "common/routing"
 
 export const Page404 = () => {
   return (
-    <>
+    <div className={s.container}>
       <h1 className={s.title}>404</h1>
       <h2 className={s.subTitle}>page not found</h2>
-    </>
+      <Button variant="contained" size="large" color={"primary"} component={Link} to={PATH.MAIN}>
+        Main page
+      </Button>
+    </div>
   )
 }
